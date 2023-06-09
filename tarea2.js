9fd54c4780662a6ee0c9771afeb9fcbecc3a8c40
DiegoMolina <andanikfdiego@gmail.com>
2023-06-09 14:22:01 -0500

    Tarea2 openbootcamp

---
+++ tarea2.js
@@ -0,0 +1,17 @@
+const nombre = ["Diego Molina"];
+console.log(nombre)
+
+const edad = [45];
+console.log(edad)
+SoyDesarrollador = false;
+console.log(SoyDesarrollador)
+const fecha_nacimiento = new Date("Julio 16 1978");
+console.log(fecha_nacimiento)
+const libroFavorito = {
+    titulo: "Eclesiastes",
+    autor: "Salomon",
+    fecha: 935 + " a.C",
+    URL : "https://www.biblegateway.com/passage/?search=Eclesiast%C3%A9s%201&version=RVR1960"
+    
+}
+console.log(libroFavorito.fecha)
